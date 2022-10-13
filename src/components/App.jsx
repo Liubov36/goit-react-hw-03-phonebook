a044b12354d4d5abb2f081a23234ca99f5fcabc4
liuba36 <inkkli24@gmail.com>
2022-10-13 19:47:53 +0200

    add change finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,7 +18,7 @@ export class App extends Component {
     const { contacts } = this.state;
     const newContact = { id: nanoid(), name, number };
 
-    contacts.some(contact => contact.name === name)
+    contacts.some(contact => contact.name.toLowerCase() === name.toLowerCase())
       ? Report.warning(
           `${name}`,
           'This user is already in the contact list.',
